82d49408a2cf4ddd41980938090b80eccba9abe6
Samilly Ellen de Castro <samilly.castro@escolar.ifrn.edu.br>
2025-02-21 11:25:49 -0300

    Update login.php
    tem erros de sintaxe e atributos que precisam ser ajeitados

--- login.php
+++ login.php
@@ -1,3 +1,43 @@
+<?php
+include('config.php');
+
+if (isset($_POST['username']) && isset($_POST['senha'])) {
+    //quantidade de caracteres
+    if(strlen($_POST['username']) == 0){
+        echo "preencha seu username";
+    }else if (strlen($_POST['senha']) == 0){
+        echo "preencha seu senha";
+    }else{
+        //testar com o CON se n funcionar com newPDO
+        $username = $con -> real_scap_string ($_POST ['username']);
+        $senha = $con -> real_scap_string ($_POST ['senha']);
+
+        $sql_code = "SELECT * FROM usuario WHERE username = '$username' AND senha = '$senha'";
+        $sql_query = $con -> query ($sql_code) or die("falha na execução");
+
+        $quantidade = $sql_query -> num_rows;
+
+        if($quantidade==1){
+            //vai ser pegos os dados e jogados na variavel username
+            $username = $sql_query-> fetch_assoc();
+
+            if(!isset($_SESSION)){
+                session_start();
+            }
+
+            $_SESSION ['username'] = $username ['username'];
+            header("Location: home.php");
+
+        }
+        else{
+            echo "falha ao logar. seu email ou senha estao incorretos";
+        }
+    }
+} 
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,11 +46,11 @@
     <title>tela de login</title>
 </head>
 <body>
-    <a href="home.php">voltar</a>
+    <a href="parte1.php">voltar</a>
     <div>
         <h1>login</h1>
-        <form action="teste_login.php" method="post">
-            <input type="text" name="nome" placeholder="username">
+        <form action="" method="POST">
+            <input type="text" name="username" placeholder="username">
             <br><br>
             <input type="password" name="senha" placeholder="senha">
             <br><br>
